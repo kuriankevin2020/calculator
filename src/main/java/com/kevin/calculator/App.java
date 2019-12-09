e0e91a058f0208ba8dc28de175698c2dfc4703f1
Kurian Kevin <kuriankevin2020@gmail.com>
2019-12-09 22:42:24 +0530

    TestClass_cleanup

--- src/main/java/com/kevin/calculator/App.java
+++ src/main/java/com/kevin/calculator/App.java
@@ -1,13 +1,7 @@
-package com.kevin;
+package com.kevin.calculator;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
